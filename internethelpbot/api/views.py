26beb2e8d003645c96a701ad092a77b8a2e162e7
Chinmaya Krishnan Mahesh <chinmaya.mahesh@gmail.com>
2016-12-09 22:55:22 +0530

    Created API

---
+++ internethelpbot/api/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+import json
+from watson_developer_cloud import ConversationV1
+import os
+
+# Create your views here.
+
+class message(APIView):
+
+    def get(self, request, *args, **kw):
+        msg = request.GET.get('message', None)
+        contextstr = request.GET.get('context', None)
+        contextjson = json.loads(contextstr)
+        workspace_id = os.environ['IBMWID']
+        conversation = ConversationV1(
+                  username=os.environ['IBMUNAME'],
+                  password=os.environ['IBMPASS'],
+                  version='2016-09-20'
+        )
+        response = conversation.message(
+                workspace_id=workspace_id,
+                message_input={'text': msg},
+                context=contextjson
+        )
+
+        result = Response(response, status=status.HTTP_200_OK)
+        return result
