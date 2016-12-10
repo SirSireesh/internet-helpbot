474363c4cebd2137bbc9b1c5ced1c75e413fbcde
Siressh <sireeshkodali1@gmail.com>
2016-12-10 11:08:18 +0530

    Save

--- get_step.py
+++ get_step.py
@@ -1,18 +1,43 @@
 #!/usr/bin/env python3.4
 
 import os
+from subprocess import call
 
 def search(key):
     it = 0
     f = open('index.php', 'r')
     lines = f.readlines()
     it = 0
+    img = []
     for i in lines:
         if '===' in i:
             if key in i:
                 while '===' not in lines[it + 1]:
                     print(lines[it])
+                    if '[[' in lines[it]:
+                        img = lines[it]
                     it += 1
+
+                it1 = 0
+                print(img)
+                for c in img:
+                    if c != '[':
+                        it1 += 1
+                    else :
+                        break
+                img = img[it1 + 1:]
+                it1 = 0
+                for c in img:
+                    if c != '|':
+                        it1 += 1
+                    else:
+                        break
+                img = img[:it1]
+                img = img.replace(' ', '-') 
+            print("The imgae is : ", img)
+
+#        call(["open",'/Volumes/Data/Temp/GMail/' +  str(img)])
+
         it += 1
 
 search("Sending")
